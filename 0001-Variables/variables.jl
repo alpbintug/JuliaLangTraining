5220a4ee80803a8e8e3840320c2260d45a84d035
alpbintug <alp.bintug@gmail.com>
2020-08-27 13:50:40 +0300

    initial commit

---
+++ 0001-Variables/variables.jl
@@ -0,0 +1,17 @@
+x = 3+11
+
+print("x: ", x, "\n") 
+# \n character is newline character in Julia too!
+
+
+x/=4
+
+#= MULTI
+LINE
+COMMENTS
+OMG
+SUCK THIS PYTHON
+=#
+print("x*pi:" ,x*pi, "\n") #pi is predefined like on MATLAB
+
+print("sqrt(x):", sqrt(x)) #sqrt is built in, great
---
+++ README.md
@@ -0,0 +1,4 @@
+# Julia Language Training
+
+I heard that Julia is a new and COOL and fast, so I decided to play with it.
+Here lies my codes on Julia.
